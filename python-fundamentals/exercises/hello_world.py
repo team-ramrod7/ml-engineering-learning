f1678485fc993a0521440df68c36b45957470229
Andrew Mills <roderickmills7@outlook.com>
2026-02-17 13:05:27 -0700

    Added ch 1 work to exercises folder

---
+++ python-fundamentals/exercises/hello_world.py
@@ -0,0 +1,8 @@
+message = "Hello World"                         # Add variable named 'message'
+print(message)
+
+message = "Hello Python Crash Course World!"    # Update value of 'message'
+print(message)
+
+mesage = "Hello Python Crash Course reader"
+print(mesage)
